b80e218acab54bfcb09a98363418297a1e9bd366
nmhummel <natalie.hummel@gmail.com>
2020-10-22 20:37:43 -0500

    commit tree 05b6aac9133d810669f44251299a2c2e9ac23a21

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
+def my_collect(array)
+    counter = 0
+    new_array = []
+    while counter < array.length
+        new_value = yield(array[counter])
+        new_array.push(new_value)
+        counter +=1
+    end
+    new_array
+end
 
+array = ["jimmy", "gReta", "kylE"]
+my_collect(array) do |name| 
+    name.upcase
+end
